437287e0e91885ee083c9b4a9584873385e06e9f
oliveira-dn <daniel.lola@aluno.educa.go.gov.br>
2025-11-30 12:46:56 -0300

    Delete SITE PS2 directory

--- SITE PS2/script.js
+++
@@ -1,9 +0,0 @@
-// Sons
-const navSound = new Audio("assets/nav.mp3");
-const selectSound = new Audio("assets/select.mp3");
-
-// Para botões de menu
-document.querySelectorAll(".menu-btn").forEach(btn => {
-    btn.addEventListener("mouseenter", () => navSound.play());
-    btn.addEventListener("click", () => selectSound.play());
-});
